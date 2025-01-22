cca31a85ac90f7ccec8597412932c7bba3d2aa5b
Jack Lonergan <jlonergan@login1.obshpc.carnegiescience.edu>
2025-01-22 10:47:10 -0800

    code has section at bottom to plot SMF's for emulator/galacticus/empirical models

--- subhalos_inference.py
+++ subhalos_inference.py
@@ -30,10 +30,8 @@ for i in range(len(lines)):
         lines[i] = lines[i][1:-2]
         lines[i].split()
         lines[i] = [float(i) for i in lines[i].split()]
-        #print('lines[' + str(i) + ']: ', lines[i])
     else:
         lines[i] = float(lines[i])
-        #print('lines[' + str(i) + ']: ', lines[i])
 
 # put in conditions to prevent from crashing (min_val less than max_val/ sizes of data_min data_max)
 def norm_transform_inv(norm_data, data_min, data_max, min_val = -1 , max_val = 1):
@@ -172,6 +170,7 @@ def emulator_data(emulator = emulator, lines = lines, num_iterations = 1):
     s_I = 0.18
     p = 1/(1 + countSubhalosMean*s_I**2)
     r = 1/s_I**2
+    sigma = np.sqrt(r*(1 - p)/p**2)
 
     # initializing arrays to go from latent space to data
     i = 0 # used to count number of iterations
@@ -190,14 +189,17 @@ def emulator_data(emulator = emulator, lines = lines, num_iterations = 1):
     prob = stats.nbinom.pmf(z,r,p)
     prob = np.nan_to_num(prob)
     N = np.random.choice(z, p = prob)
+    N = 688
+    C_avg = 25.8 # normalization factor when N = mu = 1083
+    #N = C_avg * N
     data = np.array([])
 
     min_concentration =  3.4845380492242852 # minimum concentration value from Galacticus data
     while n < num_iterations:
-        samples = emulator.distribution.sample(sample_amount*N)
+        samples = emulator.distribution.sample(N)
         x, _ = emulator.predict(samples, batch_size=65336)
         xt = norm_transform_inv(x, data_min, data_max, -1, 1)
-        clip = (xt[:,0] > np.log10(2.0*massResolution/massTree)) & (xt[:,0] < np.log10(1e9/massHost)) & (xt[:,2] <= 0.0) & (xt[:,2] > -xt[:,0]+np.log10(massResolution/massTree)) & (xt[:,3] >= 0.0) & (xt[:,3] >= 0.34) & (xt[:,1] >= min_concentration)
+        clip = (xt[:,0] > np.log10(2.0*massResolution/massTree)) & (xt[:,2] <= 0.0) & (xt[:,2] > -xt[:,0]+np.log10(massResolution/massTree)) & (xt[:,3] >= 0.5) & (xt[:,2] < np.log10(1e9/(massHost * 10**xt[:,0])))
 
         if len(data) > N:
             data = data[:int(N)]
@@ -215,8 +217,8 @@ def emulator_data(emulator = emulator, lines = lines, num_iterations = 1):
             pass
         if isinstance(data[0], float):
             reg_massInfall.append(massHost * (10**data[0]))
-            reg_massBound = [np.array(reg_massInfall[0] * 10**data[2])]
             reg_concentration.append(data[1])
+            reg_massBound = [np.array(reg_massInfall[0] * 10**data[2])]
             reg_redshift.append(data[3])
             reg_orbitalRadius.append(radiusVirialHost * (10**data[4]))
             reg_truncationRadius.append(radiusVirialHost * (10**data[5]))
@@ -255,41 +257,34 @@ def emulator_data(emulator = emulator, lines = lines, num_iterations = 1):
 
     return reg_massInfall, reg_concentration, reg_massBound, reg_redshift, reg_orbitalRadius, reg_truncationRadius, np.array(reg_x), np.array(reg_y)
 
-#test = emulator_data()
-#f = h5py.File('em_y_data.hdf5', 'w')
-#f.create_dataset('r2d', data = test[-1])
-#f.close()
-#jkl
-
 # Constructing initialized parameters
-#output_path = os.getcwd() + '/emulator_inference_output_' + dm_model + '/' #CHANGE THIS WHEN WORKING ON STANDARD OR EMULATOR
-output_path = os.getcwd() + '/test/'
+#output_path = os.getcwd() + '/emulator_inference_output_WDM_smooth/' #CHANGE THIS WHEN WORKING ON STANDARD OR EMULATOR
+output_path = os.getcwd() + '/dan_test/'
 #job_index = sys.argv[2]
 job_index = 1
 #n_keep = 100
 n_keep = 2
 summary_statistic_tolerance = 1e5
 
-from samana.Data.b1422 import B1422_HST
-from samana.Model.b1422_model import B1422ModelEPLM3M4Shear
-data_class = B1422_HST()
-model = B1422ModelEPLM3M4Shear
-preset_model_name = 'CDM'
+#from samana.Data.b1422 import B1422_HST
+#from samana.Model.b1422_model import B1422ModelEPLM3M4Shear
+from samana.Data.Mocks.baseline_smooth_mock import BaselineSmoothMockModel
+from samana.Data.Mocks.baseline_smooth_mock import BaselineSmoothMock
+data_class = BaselineSmoothMock()
+model = BaselineSmoothMockModel
+preset_model_name = 'WDMEmulator'
 
 kwargs_sample_realization = {}
 kwargs_sample_realization['LOS_normalization'] = ['FIXED', 0.]
 kwargs_sample_realization['log_m_host'] = ['FIXED', 13.3]
 kwargs_sample_realization['cone_opening_angle_arcsec'] = ['FIXED', 8.0]
-#kwargs_sample_realization['sigma_sub'] = ['FIXED', 3/5 * 0.357]
-kwargs_sample_realization['sigma_sub'] = ['FIXED', 0.12]
 kwargs_sample_realization['log_mlow'] = ['FIXED', 6.0]
 kwargs_sample_realization['log_mhigh'] = ['FIXED', 9.0]
-
-# WDM specific parameter
-#kwargs_sample_realization['log_mc'] = ['UNIFORM', 4.8, 10.0] 
+kwargs_sample_realization['sigma_sub'] = ['FIXED', 0.12]
+kwargs_sample_realization['log_mc'] = ['FIXED', 7.361] 
 
 # parameter for emulator data (keep commented out when not working with emulator)
-#kwargs_sample_realization['emulator_input'] = ['FIXED', emulator_data]
+kwargs_sample_realization['emulator_input'] = ['FIXED', emulator_data]
 
 kwargs_sample_source = {'source_size_pc': ['FIXED', 5]}
 kwargs_sample_macro_fixed = {
@@ -300,14 +295,64 @@ kwargs_sample_macro_fixed = {
 kwargs_model_class = {'shapelets_order': 10} # source complexity
 
 # Run the simulation
-forward_model(output_path, job_index, n_keep, data_class, model, preset_model_name, 
-              kwargs_sample_realization, kwargs_sample_source, kwargs_sample_macro_fixed,
-              tolerance=summary_statistic_tolerance, log_mlow_mass_sheets = 6.0, kwargs_model_class = kwargs_model_class, verbose=False, test_mode=False)
+#forward_model(output_path, job_index, n_keep, data_class, model, preset_model_name, 
+#              kwargs_sample_realization, kwargs_sample_source, kwargs_sample_macro_fixed,
+#              tolerance=summary_statistic_tolerance, log_mlow_mass_sheets = 6.0, kwargs_model_class = kwargs_model_class, verbose=False, test_mode=False)
 
-f = open(output_path + 'job_'+str(job_index)+'/parameters.txt', 'r')
-param_names = f.readlines()[0]
-print('PARAMETER NAMES:')
-print(param_names)
-f.close()
+#f = open(output_path + 'job_'+str(job_index)+'/parameters.txt', 'r')
+#param_names = f.readlines()[0]
+#print('PARAMETER NAMES:')
+#print(param_names)
+#f.close()
+
+# CODE TO PRODUCE SMF PLOTS
+from pyHalo.PresetModels.wdm import WDM
+dan_masses = np.array([])
+em_masses = np.array([])
+num_realizations = 100
+
+
+num_files = 30
+gal_masses = np.array([])
+
+for i in range(num_files): # Creating array of infall masses for Galacticus subhalos
+    f = h5py.File('gal_outputs/darkMatterOnlySubHalos' + dm_model + str(i + 1) + '.hdf5', 'r')
+    mergerTreeBuildMassesGroup = f['Parameters/mergerTreeBuildMasses']
+    massResolutionGroup = f['Parameters/mergerTreeMassResolution']
+    massResolution = massResolutionGroup.attrs['massResolution']
+    isCentral = f['Outputs/Output1/nodeData/nodeIsIsolated']
+    massInfall = f['Outputs/Output1/nodeData/basicMass']
+    massBound = f['Outputs/Output1/nodeData/satelliteBoundMass']
+    subhalos = (isCentral[:] == 0) & (massInfall[:] > 2.0*massResolution)
+    gal_masses = np.concatenate((gal_masses, massBound[subhalos]))
+
+for i in range(num_realizations): # Creating array of infall masses for emulator subhalos and subhalos from Daniel's model
+    realization = WDM(z_lens = 0.5, z_source = 2.0, sigma_sub = 0.12, log_mc = 7.361, cone_opening_angle_arcsec = 8.0, log_mlow = 6.0, log_mhigh = 9.0)
+    halos = realization.halos[:]
+    dan_mass = np.array([halo.mass for halo in halos]) # masses from single subhalo population
+    dan_masses = np.concatenate((dan_masses, dan_mass))
+
+    output = emulator_data(emulator, lines, 1)
+    em_mass = output[2][0] # masses from single subhalo population
+    em_masses = np.concatenate((em_masses, em_mass))
+
+m = np.geomspace(1e6, 1e9, 500)
+gal_subhalos = np.cumsum(np.histogram(gal_masses,m)[0][::-1])[::-1]/num_files
+dan_subhalos = np.cumsum(np.histogram(dan_masses,m)[0][::-1])[::-1]/num_realizations
+em_subhalos = np.cumsum(np.histogram(em_masses,m)[0][::-1])[::-1]/num_realizations
+
+C = gal_subhalos[0]/em_subhalos[0]
+print('C: ', C)
+plt.plot(m[:-1], dan_subhalos, 'k-', label = 'Empirical Model')
+plt.plot(m[:-1],em_subhalos, 'r-', label = 'Emulator Model')
+plt.plot(m[:-1], gal_subhalos, 'b-', label = 'Galacticus')
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('$ M $')
+plt.ylabel('$ N(> M) $')
+plt.ylim(0,1e4)
+plt.title('Bound Mass Function (CDM)')
+plt.legend()
+plt.savefig('plots/test_plot.png')
 
 print('code executed!')
